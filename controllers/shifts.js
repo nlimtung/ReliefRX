e72a42dfb9bf69a8f0b63796873ca08e0a741aef
Nicole <nicole@Nicoles-MacBook-Air.local>
2022-03-26 19:00:19 -0400

    removing random logs

--- controllers/shifts.js
+++ controllers/shifts.js
@@ -3,7 +3,6 @@ const ShiftModel = require('../models/shift.js');
 
 async function create (req, res) {
   try { 
-    console.log(req.user._id) 
 
     let createdShift= await ShiftModel.create({
       user: req.user._id, 
@@ -25,10 +24,8 @@ async function create (req, res) {
 
 async function shiftIndex(req, res) {
   try{
-    // console.log(req.user._id)
     let shifts = await ShiftModel.find()
-    // console.log(shifts)
-// 
+
     res.status(200).json(shifts)        
   }
   catch(err){
@@ -38,13 +35,10 @@ async function shiftIndex(req, res) {
 }
 
 async function shiftDetails(req, res) {
-  // console.log(req.user._id) 
-// 
+
   try{
-  // console.log(req.params) 
 
     console.log ("hello")
-    // console.log (req.params)
     let shifts = await ShiftModel.find({})
     res.status(200).json(shifts)        
 
